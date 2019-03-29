8dff320320944e94bb2d5886bb510f15d568bd3e
Alexis <alexismajyambere2@gmail.com>
2019-03-29 16:56:20 +0200

    added view of accounts

--- JS/ViewAccount.js
+++ JS/ViewAccount.js
@@ -16,6 +16,28 @@ function onselectValue(){
 	}
 }
 function viewRecords(){
-	document.getElementById('legendview').style.visibility="visible";
+	document.getElementById('ViewTable');
+		if(localStorage.transactionRecords){
+		RecordsArrays=JSON.parse(localStorage.transactionRecords);
+	}
+for(var i=0; i<RecordsArrays.length;i++){
+	tablecells(RecordsArrays[i].NoT,RecordsArrays[i].dateT,
+		RecordsArrays[i].typeT,RecordsArrays[i].accountT,
+		RecordsArrays[i].amountT,RecordsArrays[i].balanceT);
+}
+document.getElementById('legendview').style.visibility="visible";
+}
 
+function tablecells(No,date,type,account,cashier,amount,balance){
+	var tab=document.getElementById("ViewTable");
+	var newrow=tab.insertRow();
+
+	newrow.insertCell(0).innerHTML=No;
+	newrow.insertCell(1).innerHTML=date;
+	newrow.insertCell(2).innerHTML=type;
+	newrow.insertCell(3).innerHTML=account;
+	newrow.insertCell(4).innerHTML=cashier;
+	newrow.insertCell(5).innerHTML=amount;
+	newrow.insertCell(6).innerHTML=balance;
+	newrow.insertCell(7).innerHTML='<input type="button" class="buttEdit" value="Activate"/></br> <input onclick="deleteRecord()" type="button" class="buttDel" value="Delete" />';
 }
